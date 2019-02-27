14fd3e9b530acd80d3ca08ae98991dd70545d939
Ester Fernandez <esterfernand95@gmail.com>
2019-02-27 23:04:21 +0100

    create the pages and call the api

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +10,47 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  const data = {
+    title: 'Esto es un título',
+    subtitle: 'Segundo título'
+  };
+  punkAPI.getBeers()
+    .then(beers => {
+      data.beers = beers;
+      console.log(data.beers);
+      res.render('beers', data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
 
 app.listen(3000);
+
+// app.use((req, res, next) => {
+//   res.status(404);
+//   res.render('not-found');
+// });
+
+// // NOTE: requires a views/error.ejs template
+// app.use((err, req, res, next) => {
+//   // always log the error
+//   console.error('ERROR', req.method, req.path, err);
+
+//   // only render if the error ocurred before sending the response
+//   if (!res.headersSent) {
+//     res.status(500);
+//     res.render('error');
+//   }
+// });
+
+// module.exports = app;
