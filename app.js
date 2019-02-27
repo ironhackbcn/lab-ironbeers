209a51035fafccbb54a9cea4ec78616f377c9950
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-02-27 21:41:44 +0100

    Set up working, routes linked

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
+// const hbs = require('hbs');
+const app = express();
+const path = require('path');
+// const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+// const punkAPI = new PunkAPIWrapper();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/beers');
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
 
+app.get('/randombeers', (req, res, next) => {
+  res.render('randombeers');
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err);
+
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
 
 app.listen(3000);
+
+module.exports = app;
