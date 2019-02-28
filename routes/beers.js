86977e66fcc12a44d86b215256ecfefb1044f399
Ivan <4esoquevedoi@gmail.com>
2019-02-28 12:19:42 +0100

    random beer and beer id, missing css

--- routes/beers.js
+++ routes/beers.js
@@ -24,4 +24,24 @@ router.get('/beers', async (req, res, next) => {
   }
 });
 
+router.get('/beers/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const beerArray = await punkAPI.getBeer(id);
+    res.render('beer-detail', { beer: beerArray[0] });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/random-beer', async (req, res, next) => {
+  try {
+    const randomBeerArray = await punkAPI.getRandom();
+    console.log(randomBeerArray);
+    res.render('random-beer', { beer: randomBeerArray[0] });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
