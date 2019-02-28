693b6a9f30c5e8fedf29b694bdb4262fe0f326dc
xavivax1 <xavier.deviala@gmail.com>
2019-02-28 08:57:15 +0100

    primer push

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join( __dirname, '/views') );
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 app.listen(3000);
