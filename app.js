6faeda78891d7079000dc2797deeb5a4178feed2
Raul Delgado <raul@MacBook-Pro-de-Raul.local>
2019-02-28 09:47:16 +0100

    semi done

--- app.js
+++ app.js
@@ -1,21 +1,57 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+var indexRouter = require('./routes/index');
+var beersRouter = require('./routes/beers');
+var randomBeersRouter = require('./routes/randombeers');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', indexRouter);
+app.use('/beers', beersRouter);
+app.use('/randombeers', randomBeersRouter);
 
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  // Revisar porque ni puta ideaa
+  punkAPI.getBeers()
+    .then(response => {
+      console.log(response);
+      res.render('beers', { response: response });
+    })
+    .catch(error => console.log(error));
+});
 
+app.get('/randombeers', (req, res, next) => {
+  res.render('randombeers');
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err);
+
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
 
 app.listen(3000);
+
+module.exports = app;
---
+++ routes/beers.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+
+router.get('/beers', (req, res, next) => {
+  let theBeers;
+
+  punkAPI.getBeers()
+    .then(response => {
+      theBeers = response;
+      console.log(response);
+      res.render('beers', theBeers);
+    })
+    .catch(error => console.log(error));
+});
+
+module.exports = router;
