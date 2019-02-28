70487446a2032f9f2310797ba37ac2d13aac69a9
Adriana <adrirx5@gmail.com>
2019-02-28 11:37:40 +0100

    added routes folder and the missing information for iteration 6

--- app.js
+++ app.js
@@ -3,37 +3,14 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
+const beerRoutes = require('./routes/beers');
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-app.get('/beers', (req, res, next) => {
-  punkAPI.getBeers()
-    .then(beers => {
-      res.render('beers', { beers: beers });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
-app.get('/random-beers', (req, res, next) => {
-  punkAPI.getRandom()
-    .then(beers => {
-      res.render('random-beers', { beers: beers });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+app.use('/', beerRoutes);
 
 // -- 404 and error handler
 
