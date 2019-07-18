f8f87a4f51b01340e4b81b136f1ad3936d70b4d7
Aleix Maqueda <aleixmaqueda97@gmail.com>
2019-07-18 17:59:30 +0200

    First commit

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+app.get('/beers', (req, res, next) => {
+  const beers = punkAPI.getBeers()
+    .then(beersArr => {
+      console.log(beersArr);
+      res.render('beers', { beersArr });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(3000);
