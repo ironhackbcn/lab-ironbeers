1d12c938d4c216e1c69b4b41dfb360cbbe157391
Pol Ribas <polribasrovira@gmail.com>
2019-07-18 16:30:10 +0200

    piero & pol rock ot

--- app.js
+++ app.js
@@ -9,9 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+      console.log(beers);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beer => {
+      console.log(beer);
+      const final = beer[0];
+      res.render('random-beer', final);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.listen(3000);
