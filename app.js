4881611d9e382b493887aa67eba9ff10b6a8ddc6
Jorge <jorrillop@hotmail.com>
2019-07-18 19:44:12 +0200

    Function getRandom y getBeers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+'use strict';
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
@@ -8,7 +8,8 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
@@ -16,8 +17,16 @@ app.get('/', (req, res, next) => {
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers()
     .then(beers => {
-      console.log(beers);
-      res.render('beers', beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beer', { beers });
     })
     .catch(error => {
       console.log(error);
