33518ccea9d879e27bf62da35d6a6472686fb412
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-07-18 17:56:22 +0200

    link beers

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.listen(3000);
