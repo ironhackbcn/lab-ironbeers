bd61648dde6b25e745935454358831842640adf9
marcbarrera <barreramarinmarc@gmail.com>
2019-07-18 18:18:21 +0200

    first 3 points

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.set('views', (path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, '/public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  const home = {
+    beer_src: '/images/beer.png',
+    check_beer: 'Check the Beers!!',
+    random_beer: 'Check a Random Beer!',
+    check_beer_link: '/beers',
+    random_beer_link: '/random-beers'
+  };
+  res.render('index', home);
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
 
+    .then(beers => {
+      console.log(beers);
+      res.render('beers', beers);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  res.render('index');
+});
 
 app.listen(3000);
