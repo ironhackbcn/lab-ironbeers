c7eac0c1fcaba269296d24564f1ea642a971e7a8
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-07-19 11:01:50 +0200

    refactoring

--- app.js
+++ app.js
@@ -2,39 +2,16 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+const index = require('./routes/index.js');
+const beerRouter = require('./routes/beers.js');
 
-app.get('/beers', (req, res, next) => {
-  punkAPI.getBeers()
-    .then(beers => {
-      console.log(beers);
-      res.render('beers', { beers });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
-app.get('/randomBeer', (req, res, next) => {
-  punkAPI.getRandom()
-    .then(beers => {
-      console.log(beers);
-      res.render('randomBeer', { beers });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+app.use('/', index);
+app.use('/beers', beerRouter);
 
 app.listen(3000);
