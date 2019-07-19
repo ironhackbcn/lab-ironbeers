4ad4ca1566d6ca22abaeff6ea40582ec621d3bd8
Marce Ramirez <marceram87@gmail.com>
2019-07-19 09:06:37 +0200

    Almost done

--- app.js
+++ app.js
@@ -7,11 +7,33 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname + '/views'));
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then(randBeer => {
+      console.log(randBeer[0]);
+      res.render('randomBeers', randBeer[0]);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 app.listen(3000);
