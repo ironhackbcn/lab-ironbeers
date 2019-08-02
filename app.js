28afe1ec167b5aff689f2ab6f42fbcee7cebdbc2
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-08-02 16:36:31 +0200

    "menu setting"

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,10 +9,14 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/', (request, response, next) => {
+  response.render('index');
+});
+app.get('/beers', (request, response, next) => {
+  response.render('beers');
+});
+app.get('/random-beers', (request, response, next) => {
+  response.render('random-beers');
 });
 
 
