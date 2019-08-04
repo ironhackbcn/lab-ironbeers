fa53fa54b84c5bfe02057297534738fb39c8deb6
Jofre Moreno <jofremoreno@gmail.com>
2019-08-04 14:17:33 +0200

    ADD ESlint- UPDATE link buttons

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -11,23 +11,21 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-app.get("/index", (req, res, next) => {
-  res.render("index");
+app.get('/index', (req, res, next) => {
+  res.render('index');
 });
 
-app.get("/beers", (req, res, next) => {
-  res.render("beers");
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
 });
 
-app.get("/random-beers", (req, res, next) => {
-  res.render("random-beers");
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
 });
-
 
 
 app.listen(3000);
