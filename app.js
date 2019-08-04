3ca13c78d5c85a6091cbfa389a91df688b2d321c
Antonio Rivera <antonioriver@gmail.com>
2019-08-04 18:35:00 +0200

    firt commit

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  const beeeers = punkAPI.getBeers();
+  console.log(beeeers);
+  punkAPI.getBeers()
+    .then((beers) => {
+      res.render('beers', {
+        beers,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+    .then((beers) => {
+      console.log(beers);
+      res.render('random-beers', beers);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.get('/*', (req, res, next) => {
+  res.render('page-404');
+},);
 
 app.listen(3000);
