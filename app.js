063d3aab033677abb8d24938b03678f92b455fb1
Jofre Moreno <jofremoreno@gmail.com>
2019-08-10 13:41:15 +0200

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 
 app.get('/', (req, res, next) => {
@@ -20,12 +21,31 @@ app.get('/index', (req, res, next) => {
 });
 
 app.get('/beers', (req, res, next) => {
-  res.render('beers');
+  punkAPI
+  .getBeers()
+  .then((beers) => {
+    res.render ('beers', {beers})
+  })
+  .catch((error) => {
+    console.log (error()
+    )
+  })
 });
 
 app.get('/random-beers', (req, res, next) => {
-  res.render('random-beers');
+  punkAPI
+  .getRandom()
+  .then((beers) => {
+    res.render ('random-beers', beers[0])
+  })
+  .catch((error) => {
+    console.log (error()
+    )
+  })
 });
 
+app.get('*',(req, res, next) => {
+  res.render('404');
+});
 
 app.listen(3000);
