7d11177a029c1c36188e3a6dc765ae3ef8e38caa
Nomeolvides <info@nomeolvides.cat>
2019-08-21 15:34:23 +0200

    Iteration 3: done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable quotes */
 const express = require("express");
 const hbs = require("hbs");
@@ -14,6 +15,20 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res, next) => {
   res.render("index");
+});
+
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers()
+    // eslint-disable-next-line arrow-parens
+    .then(beers => {
+      console.log("We have 25 beers");
+      res.render("beers", { beers });
+    })
+    // eslint-disable-next-line arrow-parens
+    .catch(error => {
+      console.log("Error", error);
+    });
 });
 
 app.listen(3000);
