1f8554ee5c4a4477f78f7b3a7895a4795c454e87
Elena Piaggio <helenamercury@gmail.com>
2019-09-08 21:16:06 +0200

    iterations 1 to 5

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -10,12 +10,32 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Partials
+hbs.registerPartials(__dirname + '/views/partials');
 
-
+/******* RUTAS ********/
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
 
+  let prova = {
+    cities: ["Miami", "Madrid", "Barcelona", "Paris", "México", "Berlín"]
+  }
+
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(Object.values(beers));
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log("This is the error; ", error)
+    })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beer');
+});
 
 app.listen(3000);
